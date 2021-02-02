da8acdd614765b107b56c5ac816fc356488733f9
fcastr0 <freddycastro916@gmail.com>
2021-02-02 12:43:23 -0500

    added function to test JavaScript and console log

--- tutorial1_getting_started/main.js
+++ tutorial1_getting_started/main.js
@@ -3,3 +3,24 @@ var b = 6;
 var c = a * b;
 console.log(c)
 console.log("Hey! You're not suppose to see this!")
+function testSize(num) {
+    if (num < 5) {
+      return "Tiny"
+    }
+    else if (num < 10) {
+      return "Small"
+    }
+    else if (num < 15) {
+      return "Medium"
+    }
+    else if (num < 20) {
+      return "Large"
+    }
+    else {
+      return "Huge"
+    }
+  
+    return "Change Me";
+  }
+  
+  console.log(testSize(7));
