78eae580024bdb64ce527717faae88d4e1ec8a64
fcastr0 <freddycastro916@gmail.com>
2021-02-18 21:29:50 -0500

    Finalized graph

--- tutorial2_quantities_and_amounts/main.js
+++ tutorial2_quantities_and_amounts/main.js
@@ -5,16 +5,16 @@ d3.csv('../data/squirrelActivities.csv', d3.autoType)
     /** CONSTANTS */
     const width = window.innerWidth * .8;
     const height = window.innerHeight;
-    const margins = { top: 10, bottom: 25, left: 10, right: 10 };
+    const margins = { top: 20, bottom: 25, left: 50, right: 150 };
 
     const xScale = d3.scaleLinear()
       .domain([0, d3.max(data, d => d.count)])
-      .range([0, width])
+      .range([0, width - margins.right - margins.left])
       .interpolate(d3.interpolateRound)//what does this exactly do?
 
     const yScale = d3.scaleBand()
       .domain(data.map(d => d.activity))
-      .range([height, 0])
+      .range([margins.top, height - margins.bottom])
       .paddingInner(.4)
       .round(true) //Not sure if this makes a difference or what it does
 
@@ -28,21 +28,21 @@ d3.csv('../data/squirrelActivities.csv', d3.autoType)
     svg.selectAll("rect")
       .data(data)
       .join("rect")
-      .attr("width", (d, i) => width - xScale(d.count))
+      .attr("width", (d, i) => xScale(d.count))
       .attr("height", yScale.bandwidth()) //what does the bandwidth actually do?
       .attr("fill", "orange") // try changing this to be a dynamic colorscale
       .attr("y", d => yScale(d.activity))
-      .attr("x", d => 0) //why doesnt xScale(d.count) work? why does it cause it to flip?
+      .attr("x", d => margins.left) //why doesnt xScale(d.count) work? why does it cause it to flip?
 
     // draw bottom (left) 'activity' text
     svg.selectAll("text.activity")
       .data(data)
       .join("text")
       .attr("class", 'activity')
-      .attr("x", d => 30)
+      .attr("x", d => margins.left)
       .attr("y", d => yScale(d.activity))
-      .attr("dy", "10em") // adjust the text a bit lower down
-      .attr("text-anchor", 'middle') // set the x/y to refer to the middle of the word
+      .attr("dy", "-.25em") // adjust the text a bit lower down
+      .attr("text-anchor", 'right') // set the x/y to refer to the middle of the word
       .text(d => d.activity) // set the text
 
     // draw top 'count' text
@@ -50,9 +50,9 @@ d3.csv('../data/squirrelActivities.csv', d3.autoType)
       .data(data)
       .join("text")
       .attr("class", 'count')
-      .attr("x", d => 450) //
-      .attr("y", d => xScale(d.count))
-      .attr("dx", "10em") // adjust the text a bit lower down
-      .attr("text-anchor", 'middle') // set the x/y to refer to the middle of the word
+      .attr("x", d => xScale(d.count) + margins.left) //
+      .attr("y", d => yScale(d.activity) + yScale.bandwidth() / 2)
+      .attr("dx", ".25em") // adjust the text a bit lower down
+      .attr("text-anchor", 'start') // set the x/y to refer to the middle of the word
       .text(d => d3.format(",")(d.count)) // set the text, add a formatter to properly format numbers: https://github.com/d3/d3-format
   })
