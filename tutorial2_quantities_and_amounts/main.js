2d3a9d6d3e7e2de156a8873c2299e5514d7323c9
fcastr0 <freddycastro916@gmail.com>
2021-02-22 11:34:43 -0500

    Added color

--- tutorial2_quantities_and_amounts/main.js
+++ tutorial2_quantities_and_amounts/main.js
@@ -4,55 +4,54 @@ d3.csv('../data/squirrelActivities.csv', d3.autoType)
 
     /** CONSTANTS */
     const width = window.innerWidth * .8;
-    const height = window.innerHeight;
-    const margins = { top: 20, bottom: 25, left: 50, right: 150 };
+    const height = window.innerHeight / 2;
+    const margins = { top: 30, bottom: 25, left: 25, right: 150};
+    const color = d3.scaleSequential()
+      .domain([0, d3.max(data, d => d.count)])
+      .interpolator(d3.interpolatePurples)
 
     const xScale = d3.scaleLinear()
       .domain([0, d3.max(data, d => d.count)])
       .range([0, width - margins.right - margins.left])
-      .interpolate(d3.interpolateRound)//what does this exactly do?
+      .interpolate(d3.interpolateRound)
 
     const yScale = d3.scaleBand()
       .domain(data.map(d => d.activity))
       .range([margins.top, height - margins.bottom])
-      .paddingInner(.4)
-      .round(true) //Not sure if this makes a difference or what it does
+      .paddingInner(.35)
+      .round(true)
 
-    /** DRAWING ELEMENTS */
     const svg = d3.select('#barchart-container')
-      .append("svg") // this is our drawing space
+      .append("svg") 
       .attr("width", width)
       .attr("height", height)
 
-    // draw rectangles
     svg.selectAll("rect")
       .data(data)
       .join("rect")
       .attr("width", (d, i) => xScale(d.count))
-      .attr("height", yScale.bandwidth()) //what does the bandwidth actually do?
-      .attr("fill", "orange") // try changing this to be a dynamic colorscale
+      .attr("height", yScale.bandwidth()) 
       .attr("y", d => yScale(d.activity))
-      .attr("x", d => margins.left) //why doesnt xScale(d.count) work? why does it cause it to flip?
+      .attr("x", d => margins.left)
+      .attr("fill", d => color(d.count))
 
-    // draw bottom (left) 'activity' text
     svg.selectAll("text.activity")
       .data(data)
       .join("text")
       .attr("class", 'activity')
       .attr("x", d => margins.left)
       .attr("y", d => yScale(d.activity))
-      .attr("dy", "-.25em") // adjust the text a bit lower down
-      .attr("text-anchor", 'right') // set the x/y to refer to the middle of the word
-      .text(d => d.activity) // set the text
+      .attr("dy", "-.25em") 
+      .attr("text-anchor", 'right') 
+      .text(d => d.activity)
 
-    // draw top 'count' text
     svg.selectAll("text.count")
       .data(data)
       .join("text")
       .attr("class", 'count')
       .attr("x", d => xScale(d.count) + margins.left) //
       .attr("y", d => yScale(d.activity) + yScale.bandwidth() / 2)
-      .attr("dx", ".25em") // adjust the text a bit lower down
-      .attr("text-anchor", 'start') // set the x/y to refer to the middle of the word
+      .attr("dx", ".10em")
+      .attr("text-anchor", 'start')
       .text(d => d3.format(",")(d.count)) // set the text, add a formatter to properly format numbers: https://github.com/d3/d3-format
   })
