f6b0f49c1598313b35348f1051069059b2502576
fcastr0 <freddycastro916@gmail.com>
2021-02-24 00:09:30 -0500

    added axes and svg

--- tutorial3_distributions/main.js
+++ tutorial3_distributions/main.js
@@ -1,7 +1,7 @@
 /* CONSTANTS AND GLOBALS */
 const width = window.innerWidth * 0.7,
-  height = window.innerHeight * 0.7,
-  margin = { top: 20, bottom: 60, left: 60, right: 40 },
+  height = window.innerHeight * 0.80,
+  margin = { top: 20, bottom: 60, left: 60, right: 60 },
   radius = 5;
 
 // these variables allow us to access anything we manipulate in init() but need access to in draw().
@@ -17,7 +17,7 @@ let state = {
 };
 
 /* LOAD DATA */
-d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
+d3.csv("../data/usaheight.csv", d3.autoType).then(raw_data => {
   // + SET YOUR DATA PATH
   console.log("data", raw_data);
   // save our data to application state
@@ -28,19 +28,37 @@ d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
 /* INITIALIZING FUNCTION */
 // this will be run *one time* when the data finishes loading in
 function init() {
-  // + DEFINE SCALES
+  console.log('State:', state)
+  //SCALES
+    const xScale = d3.scaleLinear()
+      .domain(d3.extent(state.data, d => d.meanheight))
+      .range([margin.left, width - margin.right])
+  //console.log("xScale:", xScale, xScale, xScale(142.6888017))
 
-  // + DEFINE AXES
+    const yScale = d3.scaleLinear()
+      .domain(d3.extent(state.data, d => d.year))
+      .range([height - margin.bottom, margin.top]) //our min value is at the bottom, max value is at the top of our svg 
 
-  // + UI ELEMENT SETUP
-  // + add dropdown options
-  // + add event listener for 'change'
+  //AXES
+    const xAxis = d3.axisBottom(xScale)
+    const yAxis = d3.axisLeft(yScale)
 
-  // + CREATE SVG ELEMENT
+  //Creating SVG
+    const svg = d3.select("#d3-container")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
 
-  // + CREATE AXES
+    svg.append("g")
+      .attr("class", "xAxis")
+      .attr("transform", `translate(${0}, ${height - margin.bottom})`)
+      .call(xAxis)
 
-  // draw(); // calls the draw function
+      svg.append("g")
+      .attr("class", "yAxis")
+      .attr("transform", `translate(${margin.left}, ${0})`)
+      .call(yAxis)
+
 }
 
 /* DRAW FUNCTION */
