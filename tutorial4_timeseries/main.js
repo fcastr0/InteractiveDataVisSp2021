7706d5e6c0812388a61ce9353cf6cc049436a5e9
fcastr0 <freddycastro916@gmail.com>
2021-03-04 14:39:06 -0500

    missing line and values

--- tutorial4_timeseries/main.js
+++ tutorial4_timeseries/main.js
@@ -1,7 +1,7 @@
 /* CONSTANTS AND GLOBALS */
-const width = window.innerWidth * 0.7,
-  height = window.innerHeight * 0.7,
-  margin = { top: 20, bottom: 50, left: 60, right: 40 },
+const width = window.innerWidth * 0.95,
+  height = window.innerHeight * 0.90,
+  margin = { top: 20, bottom: 65, left: 60, right: 30 },
   radius = 5;
 
 // these variables allow us to access anything we manipulate in init() but need access to in draw().
@@ -9,18 +9,22 @@ const width = window.innerWidth * 0.7,
 let svg;
 let xScale;
 let yScale;
+let xAxis;
+let yAxis;
+let xAxisGroup;
+let yAxisGroup;
 
 /* APPLICATION STATE */
 let state = {
   data: [],
-  selection: "All", // + YOUR FILTER SELECTION
+  selection: "All Prices", // + YOUR FILTER SELECTION
 };
 
 /* LOAD DATA */
 // + SET YOUR DATA PATH
-d3.csv('YOUR_DATA_PATH', d3.autoType)
+d3.csv("../data/gasprices.csv", d3.autoType)
   .then(data => {
-    console.log("loaded data:", data);
+    console.log("LOADED DATA:", data);
     state.data = data;
     init();
   });
@@ -28,35 +32,124 @@ d3.csv('YOUR_DATA_PATH', d3.autoType)
 /* INITIALIZING FUNCTION */
 // this will be run *one time* when the data finishes loading in
 function init() {
-  // + SCALES
+// SCALES
+xScale = d3.scaleTime()
+  .domain(d3.extent(state.data, function(d) {  //why does this work but the .domain at the bottom doesnt?
+    return new Date(d.date)
+}))
+  //.domain(d3.extent(state.data, d => d.date))
+  .range([margin.left, width - margin.right])
+  //console.log("xScale is:", xScale, xScale(1/2/1995))
 
-  // + AXES
+yScale = d3.scaleLinear()
+  .domain(d3.extent(state.data, d => d.r1)) // to get the scale on the y-axis I think we need to map to something that isnt r1
+  .range([height - margin.bottom, margin.top])
 
-  // + UI ELEMENT SETUP
+// AXES 
+const xAxis = d3.axisBottom(xScale)
+  .tickFormat(d3.timeFormat("%Y")) //sets it to yearly 
+  .ticks(d3.timeYear.every(1)) //this works but the ending date doesnt look good 
 
-  // add in dropdown options from the unique values in the data
+const yAxis = d3.axisLeft(yScale)
 
-  // + SET SELECT ELEMENT'S DEFAULT VALUE (optional)
+// CREATE SVG 
+svg = d3.select("#d3-container")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .style('background-color', 'none') //changes the svg background color
+  .style("font-family", "verdana")
 
-  // + CREATE SVG ELEMENT
+const xAxisGroup = svg.append("g")
+  .attr("class", "xAxis")
+  .attr("transform", `translate(${0}, ${height - margin.bottom})`)
+  .call(xAxis)
+  
+xAxisGroup.append("text")
+  .attr("class", 'axis-title')
+  .attr("x", width / 2)
+  .attr("y", 45)
+  .attr("text-anchor", "middle")
+  .attr("fill", "black") //why do we have to add a fill attribute?
+  .attr("font-size","13")
+  .text("Year")
+  // .style("font-family", "Calibri")
 
-  // + CALL AXES
+// svg.selectAll("text.date") tried to format the dates... its a no go
+//   .data(state.data)        also tried to change ticks from 2ys to 1 yr
+//   .join("text")
+//   .attr("class", 'date')
+//   .attr("x", d => xScale(d.date))
+//   .attr("y", 0)
+//   .attr("dx", "-.8em")
+//   .attr("dy", ".15em")
+//   .attr("transform", "rotate(-65)")
 
-  draw(); // calls the draw function
+const yAxisGroup = svg.append("g")
+  .attr("class", "yAxis")
+  .attr("transform", `translate(${margin.left}, ${0})`)
+  .call(yAxis)
+  
+yAxisGroup.append("text")
+  .attr("class", 'axis-title')
+  .attr("x", -40)
+  .attr("y", height / 2)
+  .attr("writing-mode", "vertical-lr")
+  .attr("text-anchor", "middle")
+  .attr("fill", "black")
+  .attr("font-size","13")
+  .text("Price in USD")
+
+const dropdown = d3.select("#dropdown")
+
+dropdown.selectAll("options")
+  .data([
+    { key: "All Prices", label: "All Prices"},
+    { key: "r1", label: "Regular"},
+    { key: "m1", label: "Midgrade"},
+    { key: "p1", label: "Premium"},
+    { key: "d1", label: "Diesel"}])
+  .join("option")
+  .attr("value", d => d.key)
+  .text(d => d.label)
+
+  dropdown.on("change", event => {
+    console.log("DROP DOWN IS CHANGED", event.target.value)
+    state.selection = event.target.value
+    console.log("NEW STATE", state)
+    draw();
+})
+
+ 
+    draw(); // calls the draw function
 }
 
 /* DRAW FUNCTION */
 // we call this everytime there is an update to the data/state
 function draw() {
+  console.log("state.selected", state.selection)
   // + FILTER DATA BASED ON STATE
+  const filteredData = state.data // this will maybe have to be an else if statement
+  .filter(d => state.selection === d.r1)
 
-  // + UPDATE SCALE(S), if needed
+  yScale.domain(d3.extent(filteredData, d => d.r1))
 
-  // + UPDATE AXIS/AXES, if needed
-
-  // + DRAW CIRCLES/LABEL GROUPS, if you decide to
-
-  // + DEFINE LINE GENERATOR FUNCTION
+  console.log("filteredData", filteredData)
 
   // + DRAW LINE AND/OR AREA
+  const lineFunction = d3.line()
+    .x(d => xScale(d.date))
+    .y(d => yScale(d.r1))
+  
+  svg.selectAll("path.line")
+    .data([filteredData])
+    .join("path")
+    .attr("class", "line")
+    .attr("fill", "none")
+    .attr("stroke", "black")
+    .attr("d", lineFunction)
+
+  //select all ()
+  // join data()
+  //render elements
 }
