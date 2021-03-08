9134a0ffd7d35c809587a8d24568003a72ea23ce
fcastr0 <freddycastro916@gmail.com>
2021-03-08 11:06:51 -0500

    Some graph and visualization issues

--- tutorial4_timeseries/main.js
+++ tutorial4_timeseries/main.js
@@ -2,7 +2,7 @@
 const width = window.innerWidth * 0.95,
   height = window.innerHeight * 0.90,
   margin = { top: 20, bottom: 65, left: 60, right: 30 },
-  radius = 5;
+  radius = 2.5;
 
 // these variables allow us to access anything we manipulate in init() but need access to in draw().
 // All these variables are empty before we assign something to them.
@@ -17,7 +17,7 @@ let yAxisGroup;
 /* APPLICATION STATE */
 let state = {
   data: [],
-  selection: "All Prices", // + YOUR FILTER SELECTION
+  selection: "Regular", // + YOUR FILTER SELECTION
 };
 
 /* LOAD DATA */
@@ -34,21 +34,19 @@ d3.csv("../data/gasprices.csv", d3.autoType)
 function init() {
 // SCALES
 xScale = d3.scaleTime()
-  .domain(d3.extent(state.data, function(d) {  //why does this work but the .domain at the bottom doesnt?
-    return new Date(d.date)
-}))
+  .domain(d3.extent(state.data, function(d) {  
+    return new Date(d.date)}))
   //.domain(d3.extent(state.data, d => d.date))
   .range([margin.left, width - margin.right])
-  //console.log("xScale is:", xScale, xScale(1/2/1995))
 
 yScale = d3.scaleLinear()
-  .domain(d3.extent(state.data, d => d.r1)) // to get the scale on the y-axis I think we need to map to something that isnt r1
+  .domain(d3.extent(state.data, d => d.d1))
   .range([height - margin.bottom, margin.top])
 
 // AXES 
 const xAxis = d3.axisBottom(xScale)
   .tickFormat(d3.timeFormat("%Y")) //sets it to yearly 
-  .ticks(d3.timeYear.every(1)) //this works but the ending date doesnt look good 
+  .ticks(d3.timeYear.every(2)) 
 
 const yAxis = d3.axisLeft(yScale)
 
@@ -57,8 +55,8 @@ svg = d3.select("#d3-container")
   .append("svg")
   .attr("width", width)
   .attr("height", height)
-  .style('background-color', 'none') //changes the svg background color
-  .style("font-family", "verdana")
+  //.style('background-color', 'none') changes the svg background color
+  //.style("font-family", "verdana") will change later
 
 const xAxisGroup = svg.append("g")
   .attr("class", "xAxis")
@@ -70,20 +68,10 @@ xAxisGroup.append("text")
   .attr("x", width / 2)
   .attr("y", 45)
   .attr("text-anchor", "middle")
-  .attr("fill", "black") //why do we have to add a fill attribute?
+  .attr("fill", "black")
   .attr("font-size","13")
   .text("Year")
   // .style("font-family", "Calibri")
-
-// svg.selectAll("text.date") tried to format the dates... its a no go
-//   .data(state.data)        also tried to change ticks from 2ys to 1 yr
-//   .join("text")
-//   .attr("class", 'date')
-//   .attr("x", d => xScale(d.date))
-//   .attr("y", 0)
-//   .attr("dx", "-.8em")
-//   .attr("dy", ".15em")
-//   .attr("transform", "rotate(-65)")
 
 const yAxisGroup = svg.append("g")
   .attr("class", "yAxis")
@@ -104,7 +92,7 @@ const dropdown = d3.select("#dropdown")
 
 dropdown.selectAll("options")
   .data([
-    { key: "All Prices", label: "All Prices"},
+  //  { key: "All Prices", label: "All Prices"},
     { key: "r1", label: "Regular"},
     { key: "m1", label: "Midgrade"},
     { key: "p1", label: "Premium"},
@@ -129,27 +117,105 @@ dropdown.selectAll("options")
 function draw() {
   console.log("state.selected", state.selection)
   // + FILTER DATA BASED ON STATE
-  const filteredData = state.data // this will maybe have to be an else if statement
-  .filter(d => state.selection === d.r1)
-
-  yScale.domain(d3.extent(filteredData, d => d.r1))
+  const filteredData = state.data
+  //.filter(d => state.selection === d.r1)
 
   console.log("filteredData", filteredData)
 
+  const dots = svg
+    .selectAll(".dot")
+    .data(filteredData, d => new Date(d.date))
+    .join(
+      enter => enter.append("g")
+        .attr("class", "dot")
+        .attr("transform", d => `translate(${xScale(new Date(d.date))}, ${yScale(d.r1)})`),
+      update => update
+        .call(upate => update.transition()
+          .duration(1500)
+          .attr("transform", d => `translate(${xScale(new Date(d.date))}, ${yScale(d.r1)})`)
+    ),
+  exit => exit.remove()
+    );
+  
+  dots.selectAll("circle")
+      .data(d => [d])
+      .join("circle")
+      .attr("r", radius)
+      .attr("fill", "purple")
+
   // + DRAW LINE AND/OR AREA
-  const lineFunction = d3.line()
-    .x(d => xScale(d.date))
+  const lineFunctionr1 = d3.line()
+    .x(d => xScale(new Date(d.date)))
     .y(d => yScale(d.r1))
   
-  svg.selectAll("path.line")
+  svg.selectAll("path.r1line")
     .data([filteredData])
     .join("path")
-    .attr("class", "line")
+    .attr("class", "r1line line")
     .attr("fill", "none")
-    .attr("stroke", "black")
-    .attr("d", lineFunction)
+    .attr("stroke", "#581845")
+    .attr("stroke-width", 2)
+    .attr("d", lineFunctionr1)
+    .classed("visible", state.selection === "r1")
 
-  //select all ()
-  // join data()
-  //render elements
+  const lineFunctionm1 = d3.line()
+    .x(d => xScale(new Date(d.date)))
+    .y(d => yScale(d.m1))
+  
+  svg.selectAll("path.m1line")
+    .data([filteredData])
+    .join("path")
+    .attr("class", "m1line line")
+    .attr("fill", "none")
+    .attr("stroke", "#581845")
+    //.attr("opacity", ".30")
+    .attr("stroke-width", 2)
+    .attr("d", lineFunctionm1)
+    .classed("visible", state.selection === "m1")
+
+  const lineFunctionp1 = d3.line()
+  .x(d => xScale(new Date(d.date)))
+  .y(d => yScale(d.p1))
+  
+  svg.selectAll("path.p1line")
+    .data([filteredData])
+    .join("path")
+    .attr("class", "p1line line")
+    .attr("fill", "none")
+    .attr("stroke", "#581845")
+    .attr("stroke-width", 2)
+    .attr("d", lineFunctionp1)
+    .classed("visible", state.selection === "p1")
+  
+  const lineFunctiond1 = d3.line()
+  .x(d => xScale(new Date(d.date)))
+  .y(d => yScale(d.d1))
+  
+  svg.selectAll("path.d1line")
+    .data([filteredData])
+    .join("path")
+    .attr("class", "d1line line")
+    .attr("fill", "none")
+    .attr("stroke", "#581845")
+    .attr("stroke-width", 2)
+    .attr("d", lineFunctiond1)
+    .classed("visible", state.selection === "d1")
+
+  const areaPathr1 = d3.area()
+  .x(d => xScale(new Date(d.date)))
+  .y0(yScale(0))
+  .y1(d => yScale(d.r1))
+
+  svg.selectAll(".area")
+  .data([filteredData])
+  .join("path")
+  .attr("class", 'area')
+  .attr("opacity", 0.5)
+  .attr("fill", "#581845")
+  .attr("stroke-width", 2)
+  .transition()
+  .duration(1000)
+  .attr("d", d => areaPathr1(d))
+  // .classed("visible", state.selection === "All Prices" || state.selection === "r1")
+
 }
